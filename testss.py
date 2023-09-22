a0a3c7c32d003ff25ebd04c73665bc930a22b708
Suparnojit Sarkar <suparnojit2026@gmail.com>
2023-09-22 19:55:14 +0530

    TESTSS

--- testss.py
+++ testss.py
@@ -1,16 +1,17 @@
 import streamlit as st 
 import dehaze
+import cv2
 def main():
     st.title("Dehazing/Desmoking")
-    uploaded_file = st.file_uploader("Upload a file", type=["jpg", "png", "jpeg", "mp4"])
+    uploaded_file = st.file_uploader("Upload a file", type=["jpg", "png", "jpeg","mp4", "mov"])
     
     if uploaded_file is not None:
-        st.video(uploaded_file, caption="Uploaded File", use_column_width=True)
+        st.image(uploaded_file, caption="Uploaded File", use_column_width=True)
         
         # Process the image when a button is clicked
         if st.button("Process File"):
             processed_file = dehaze.process_file(uploaded_file)
-            st.video(processed_file, caption="Processed Image", use_column_width=True)
+            st.image(processed_file, caption="Processed Image", use_column_width=True)
 
 if __name__ == "__main__":
     main()

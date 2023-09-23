e12f773af58f49920c530b15c97c8362b484b334
AryaMajumder38 <arya.majumder.csbs26@heritageit.edu.in>
2023-09-23 06:39:46 +0530

    changed ouotpt type

--- stremlit.py
+++ stremlit.py
@@ -5,15 +5,19 @@ import dehaze  # Import your modified dehaze.py script
 # Title and description
 st.title("Desmoking/Dehazing Web App")
 
-uploaded_image = st.file_uploader("Upload a file", type=["jpg", "png", "jpeg","mp4"])
+uploaded_image = st.file_uploader("Upload a file", type=["jpg", "png", "jpeg", "mp4"])
 
 if uploaded_image:
     st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
 if st.button("Process Image"):
     if uploaded_image is not None:
+        # Convert the uploaded image data to bytes
+        uploaded_image_bytes = uploaded_image.read()
+        
         # Provide an output path for the processed image
         output_image_path = "processed_image.jpg"
-        dehaze.dehaze(uploaded_image, output_image_path)
+        dehaze.dehaze(uploaded_image_bytes, output_image_path)
+        
         processed_image = Image.open(output_image_path)
         st.image(processed_image, caption="Processed Image", use_column_width=True)

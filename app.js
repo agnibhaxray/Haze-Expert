855aa21e346ea6d3f1436fbfe80b9b5853dea66f
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2023-09-23 08:22:26 +0530

    footer-2

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var divisor1 = document.getElementById("divisor1"),
-slider1 = document.getElementById("slider1");
-function moveDivisor() { 
-	divisor1.style.width = slider1.value+"%";
+var divisor = document.getElementById("divisor"),
+slider = document.getElementById("slider");
+function moveDivisor1() { 
+	divisor.style.width = slider.value+"%";
 }
 
 var divisor2 = document.getElementById("divisor2"),
 slider2 = document.getElementById("slider2");
-function moveDivisor() { 
+function moveDivisor2() { 
 	divisor2.style.width = slider2.value+"%";
 }
